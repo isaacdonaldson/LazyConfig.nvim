1583f29f5e8117c02f3eff0e69e387ab9b2b05f7
Isaac Donaldson <isaac.w.donaldson@gmail.com>
2024-03-14 14:23:24 -0700

    turn off relative numbering

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,9 +2,13 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- use numbers for the lines and not relative numbers
+vim.o.number = true
+vim.o.relativenumber = false
+
 -- UFO Folding Options
 vim.o.foldcolumn = "1" -- '0' is not bad
-vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 vim.o.fillchars = [[eob: ,fold: ,foldopen:>,foldsep: ,foldclose:v]]
