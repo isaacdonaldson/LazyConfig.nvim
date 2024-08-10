d57dbe26188e89b9fc13cc4445335bcc4f768a96
Isaac Donaldson <isaac.w.donaldson@gmail.com>
2024-08-09 19:14:20 -0700

    comment out parts that were causing errors

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,13 +2,5 @@ return {
   -- override conform formatting options
   {
     "stevearc/conform.nvim",
-    opts = function()
-      return {
-        format = {
-          -- change default format timeout to 5 seconds
-          timeout_ms = 250,
-        },
-      }
-    end,
   },
 }
