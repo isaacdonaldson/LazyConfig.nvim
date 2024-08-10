7fdff795641d17b2be21c1a839ebbfb349ebbc25
Isaac Donaldson <isaac.w.donaldson@gmail.com>
2024-08-09 19:13:29 -0700

    remove ufo foldingm add scroll space of 10 on either side

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,13 +6,20 @@
 vim.o.number = true
 vim.o.relativenumber = false
 
--- UFO Folding Options
-vim.o.foldcolumn = "1" -- '0' is not bad
-vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldcolumn = "1" -- "0" remove the fold column
 vim.o.foldlevelstart = 99
+vim.o.foldlevel = 99
 vim.o.foldenable = true
-vim.o.fillchars = [[eob: ,fold: ,foldopen:>,foldsep: ,foldclose:v]]
+vim.o.foldmethod = "expr"
+vim.wo.foldtext = ""
+vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.o.fillchars = [[eob: ,fold: ,foldopen:v,foldsep: ,foldclose:>]]
 
+vim.g.lazyvim_statuscolumn = {
+  folds_open = true
+}
+
+vim.opt.scrolloff = 10
 
 -- Turn off GitBlame by default
 -- vim.g.gitblame_enabled = 0
