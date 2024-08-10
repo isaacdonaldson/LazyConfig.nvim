0538d2aa8b955a47995a52e5cb5f64a260be03d0
Isaac Donaldson <isaac.w.donaldson@gmail.com>
2024-08-09 20:36:22 -0700

    removed uneeded things

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -21,17 +21,10 @@ return {
       on_create = function(t)
         vim.opt.foldcolumn = "0"
         vim.opt.signcolumn = "no"
-        -- local toggle = function()
-        --   t:toggle()
-        -- end
-        -- vim.keymap.set({ "n", "t", "i" }, "<C-'>", toggle, { desc = "Toggle terminal", buffer = t.bufnr })
-        -- vim.keymap.set({ "n", "t", "i" }, "<F7>", toggle, { desc = "Toggle terminal", buffer = t.bufnr })
       end,
 
-      shading_factor = 2,
       direction = "float",
       float_opts = { border = "rounded" },
     },
   },
-
 }
