3108595981c77d700b09d1b95cc6cd06d6e557ba
Isaac <isaac.w.donaldson@gmail.com>
2024-09-19 15:38:27 -0700

    snippets and neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,8 +1,19 @@
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
-    opts = function(_, opts)
-      opts.close_if_last_window = true
-    end,
-  }
+    opts = {
+      close_if_last_window = true,
+      filesystem = {
+        filtered_items = {
+          visible = true, -- dim hidden files
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          never_show = {
+            ".git",
+            ".DS_Store",
+          },
+        },
+      },
+    },
+  },
 }
--- lua/plugins/snippets/init.lua
+++ lua/plugins/snippets/init.lua
@@ -5,7 +5,14 @@ return {
     dependencies = { "nvim-telescope/telescope.nvim", "garymjr/nvim-snippets" },
     opts = {
       snippetDir = vim.fn.stdpath("config") .. "/lua/plugins/snippets",
-    }
+    },
   },
 
+  {
+    "garymjr/nvim-snippets",
+    opts = {
+      create_cmp_source = true,
+      search_paths = { vim.fn.stdpath("config") .. "/lua/plugins/snippets" },
+    },
+  },
 }
