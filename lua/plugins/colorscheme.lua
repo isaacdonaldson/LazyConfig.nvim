4b7b846a9e74b44c1e37b0f3e26b2e0e8ddbf175
Isaac Donaldson <isaac.w.donaldson@gmail.com>
2024-11-20 09:22:32 -0800

    add Newvick colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,12 +3,14 @@ return {
   { "ellisonleao/gruvbox.nvim" },
   { "catppuccin/nvim", name = "catppuccin" },
   { "loctvl842/monokai-pro.nvim", lazy = false }, -- Primary is loaded at startup
+  { "nuvic/flexoki-nvim", name = "flexoki" }, -- Newvicks colorscheme
 
   -- Configure LazyVim to load colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "monokai-pro",
+      -- colorscheme = "flexoki",
       -- colorscheme = "tokyonight",
       -- colorscheme = "gruvbox",
       -- colorscheme = "catppuccin",
